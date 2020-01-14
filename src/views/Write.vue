eec89464c68d2d16aa23286d690763142cb7de96
cxyxiaoyuyu <923996381@qq.com>
2020-01-14 10:25:33 +0800

    增加markdown编辑器

--- src/views/Write.vue
+++ src/views/Write.vue
@@ -1,15 +1,36 @@
 <template>
   <div class="write">
     <div class="container">
-      <mavon-editor :toolbars="markdownOption" v-model="handbook"/>
+      <mavon-editor ref="md" v-model="handbook"
+        @save=saveArticle @imgAdd="imgAdd" />
     </div>
   </div>
 </template>
 
 <script>
+// import mavonEditor from 'mavon-editor'
 import 'mavon-editor/dist/css/index.css'
 export default {
+  data () {
+    return {
+      handbook: ''
+    }
+  },
   components: {
+    // mavonEditor
+  },
+  methods: {
+    saveArticle (obj, render) {
+      console.log(obj, 'articel')
+      console.log(render)
+    },
+    imgAdd (pos, $file) {
+      console.log(pos, $file)
+      var formdata = new FormData()
+      formdata.append('image', $file)
+      // 将formdata 上传到服务器
+      this.$refs.md.$img2Url(pos, 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1248660256,3129357967&fm=26&gp=0.jpg')
+    }
   }
 }
 </script>
