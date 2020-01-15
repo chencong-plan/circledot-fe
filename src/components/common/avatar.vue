2de25313eb5dd4ab0be8b4e8c0330623f8bc532c
cxyxiaoyuyu <923996381@qq.com>
2020-01-15 15:51:59 +0800

    封装公用头像组件

---
+++ src/components/common/avatar.vue
@@ -0,0 +1,17 @@
+<template>
+    <img :src="src" alt="" width=40px height=40px>
+</template>
+
+<script>
+export default {
+  props: ['src']
+}
+</script>
+
+<style scoped>
+img {
+  border-radius: 45%;
+  justify-content: flex-end;
+  cursor: pointer;
+}
+</style>
