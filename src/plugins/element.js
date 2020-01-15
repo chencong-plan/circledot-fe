a270f3fe25d919ec6761193ac7755fc009668991
cxyxiaoyuyu <923996381@qq.com>
2020-01-15 15:51:16 +0800

    添加弹框组件

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, Message, MessageBox, Notification } from 'element-ui'
 import lang from 'element-ui/lib/locale/lang/zh-CN'
 import locale from 'element-ui/lib/locale'
 locale.use(lang)
 
 Vue.use(Button)
+
+const MsgBox = MessageBox
+Vue.prototype.$msgbox = MsgBox
+Vue.prototype.$alert = MsgBox.alert
+Vue.prototype.$confirm = MsgBox.confirm
+Vue.prototype.$prompt = MsgBox.prompt
+Vue.prototype.$notify = Notification
+Vue.prototype.$message = Message
