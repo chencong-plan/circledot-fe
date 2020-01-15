b774c29ca990b728fa3645fbc05ae13f40b80080
cxyxiaoyuyu <923996381@qq.com>
2020-01-15 17:04:47 +0800

    feat: 封装commentInput组件

--- src/components/post/main/comment.vue
+++ src/components/post/main/comment.vue
@@ -1,38 +1,13 @@
 <template>
   <div class="comment">
     <div class="text">评论</div>
-    <div class="input">
-      <div class="top">
-        <img src="@/assets/avatar1.jpg" alt width="45px" height="45px" />
-        <div contenteditable="true" placeholder="输入评论..." class="inputText" ref="ta" type="text"></div>
-      </div>
-      <div class="bottom">
-        <div class="left">
-          <svg @click=showPicker xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22">
-            <g fill="none" fill-rule="evenodd">
-              <path d="M1 1h20v20H1z" />
-              <path
-                fill="#027FFF"
-                fill-rule="nonzero"
-                d="M11 18.438a7.438 7.438 0 1 0 0-14.876 7.438 7.438 0 0 0 0 14.876zm0 1.062a8.5 8.5 0 1 1 0-17 8.5 8.5 0 0 1 0 17zM7.812 9.937a1.062 1.062 0 1 0 0-2.124 1.062 1.062 0 0 0 0 2.125zm6.375 0a1.063 1.063 0 1 0 0-2.125 1.063 1.063 0 0 0 0 2.125zM11 16.232a3.27 3.27 0 0 0 3.27-3.27H7.73a3.27 3.27 0 0 0 3.27 3.27z"
-              />
-            </g>
-          </svg>
-          <span @click=showPicker>表情</span>
-          <Picker ref='picker' v-show="pickerShow" :showSearch="showSearch" @select="addEmoji"/>
-        </div>
-        <div class="right">
-          <span>支持MarkDown语法</span>
-          <el-button type="primary" size="medium" @click="submitComment">提交</el-button>
-        </div>
-      </div>
-    </div>
+    <comment-input></comment-input>
     <comment-list :commentList="commentList"></comment-list>
   </div>
 </template>
 
 <script>
-import { Picker } from 'emoji-mart-vue'
+import commentInput from './commentInput'
 import commentList from './commentList'
 import avatar from '@/components/common/avatar'
 export default {
@@ -44,6 +19,7 @@ export default {
         { avatar: 'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1059855869,2611240339&fm=26&gp=0.jpg',
           username: '千与千寻',
           agree: 1,
+          agreeActive: false,
           content: `前言`,
           childred: [],
           time: '刚刚'
@@ -51,6 +27,7 @@ export default {
         { avatar: 'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1059855869,2611240339&fm=26&gp=0.jpg',
           username: '千与千寻',
           agree: 14,
+          agreeActive: true,
           content: `### 继承二级标题
 
 >该原型对象也有一个自己的原型对象( __proto__ ) ，层层向上直到一个对象的原型对象为 null。根据定义，null 没有原型，并作为这个原型链中的最后一个环节。
@@ -68,6 +45,7 @@ Object.prototype.__proto__ === null    // true
         { avatar: 'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1059855869,2611240339&fm=26&gp=0.jpg',
           username: '千与千寻',
           agree: 0,
+          agreeActive: false,
           content: `配合jsdoc的type typedef加上d.ts一样能让你的js代码获得类ts的智能提示体验(只是缺少了编译检查)，可以参考如下concent代码示例
           http://www.baidu.com `,
           childred: [],
@@ -77,7 +55,7 @@ Object.prototype.__proto__ === null    // true
     }
   },
   components: {
-    Picker,
+    commentInput,
     commentList
   },
   methods: {
@@ -140,55 +118,5 @@ Object.prototype.__proto__ === null    // true
   color: #8a9aa9;
   text-align: center;
 }
-.input {
-  background: #f3f3f36e;
-  display: flex;
-  flex-direction: column;
-  padding: 24px 28px 15px 20px;
-  border-radius: 2px;
-}
-.input .top {
-  display: flex;
-}
-.input .top img {
-  border-radius: 50%;
-  justify-content: flex-end;
-  cursor: pointer;
-  margin-right: 15px;
-}
 
-.input .bottom {
-  margin-top: 10px;
-}
-.input .bottom .left {
-  display: flex;
-  align-items: center;
-  padding-left: 60px;
-  float: left;
-  position: relative;
-  cursor: pointer;
-}
-.input .bottom .right {
-  float: right;
-}
-.input .bottom span {
-  color: #6190e8;
-  padding: 0 15px 0 4px;
-}
-.inputText {
-  padding: 12px 12px 10px;
-  font-size: 16px;
-  outline: none;
-  border: 1px solid #dedede;
-  border-radius: 3px;
-  flex-grow: 1;
-  border-radius: 3px;
-  overflow-y: hidden;
-  min-height: 40px;
-  font-family: sans-serif;
-  background: #fff;
-}
-.input .inputText:focus {
-  border: 1px solid #007fff;
-}
 </style>
---
+++ src/components/post/main/commentInput.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="commentInput">
+    <div class="top">
+      <img src="@/assets/avatar1.jpg" alt width="45px" height="45px" />
+      <div contenteditable="true" placeholder="输入评论..." class="inputText" ref="ta" type="text"></div>
+    </div>
+    <div class="bottom">
+      <div class="left">
+        <svg @click=showPicker xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22">
+          <g fill="none" fill-rule="evenodd">
+            <path d="M1 1h20v20H1z" />
+            <path
+              fill="#027FFF"
+              fill-rule="nonzero"
+              d="M11 18.438a7.438 7.438 0 1 0 0-14.876 7.438 7.438 0 0 0 0 14.876zm0 1.062a8.5 8.5 0 1 1 0-17 8.5 8.5 0 0 1 0 17zM7.812 9.937a1.062 1.062 0 1 0 0-2.124 1.062 1.062 0 0 0 0 2.125zm6.375 0a1.063 1.063 0 1 0 0-2.125 1.063 1.063 0 0 0 0 2.125zM11 16.232a3.27 3.27 0 0 0 3.27-3.27H7.73a3.27 3.27 0 0 0 3.27 3.27z"
+            />
+          </g>
+        </svg>
+        <span @click=showPicker>表情</span>
+        <Picker ref='picker' v-show="pickerShow" :showSearch="showSearch" @select="addEmoji"/>
+      </div>
+      <div class="right">
+        <span>支持MarkDown语法</span>
+        <el-button type="primary" size="medium" @click="submitComment">提交</el-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { Picker } from 'emoji-mart-vue'
+export default {
+  computed: {
+    Picker
+  }
+}
+</script>
+
+<style scoped>
+.commentInput {
+  background: #f3f3f36e;
+  display: flex;
+  flex-direction: column;
+  padding: 24px 28px 15px 20px;
+  border-radius: 2px;
+}
+.commentInput .top {
+  display: flex;
+}
+.commentInput img {
+  border-radius: 50%;
+  justify-content: flex-end;
+  cursor: pointer;
+  margin-right: 15px;
+}
+
+.commentInput .bottom {
+  margin-top: 10px;
+}
+.commentInput .bottom .left {
+  display: flex;
+  align-items: center;
+  padding-left: 60px;
+  float: left;
+  position: relative;
+  cursor: pointer;
+}
+.commentInput .bottom .right {
+  float: right;
+}
+.commentInput .bottom span {
+  color: #6190e8;
+  padding: 0 15px 0 4px;
+}
+.inputText {
+  padding: 12px 12px 10px;
+  font-size: 16px;
+  outline: none;
+  border: 1px solid #dedede;
+  border-radius: 3px;
+  flex-grow: 1;
+  border-radius: 3px;
+  overflow-y: hidden;
+  min-height: 40px;
+  font-family: sans-serif;
+  background: #fff;
+}
+.commentInput .inputText:focus {
+  border: 1px solid #007fff;
+}
+</style>
